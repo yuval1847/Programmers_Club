e33b188f5b999d1465e330ab936e6672226bba02
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2024-06-12 19:06:53 +0300

    Update server.py

--- Networking/Files_Managment/CLI_Version/server.py
+++ Networking/Files_Managment/CLI_Version/server.py
@@ -5,120 +5,79 @@ class Server:
     """A class which represent a server."""
 
     def __init__(self, directoryPath):
-        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.serverSocket.bind(("0.0.0.0", 1234))
-        self.serverSocket.listen()
-        self.directoryDatabasePath = directoryPath
-        self.currentFileName = None
-        self.clientSocket, self.clientAddress = self.server_socket.accept()
-
         print("███████╗██╗██╗     ███████╗    ███████╗███████╗██████╗ ██╗   ██╗███████╗██████╗\n" 
               "██╔════╝██║██║     ██╔════╝    ██╔════╝██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗\n"
               "█████╗  ██║██║     █████╗      ███████╗█████╗  ██████╔╝██║   ██║█████╗  ██████╔╝\n"
               "██╔══╝  ██║██║     ██╔══╝      ╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██╔══╝  ██╔══██╗\n"
               "██║     ██║███████╗███████╗    ███████║███████╗██║  ██║ ╚████╔╝ ███████╗██║  ██║\n"
-              "╚═╝     ╚═╝╚══════╝╚══════╝    ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝\n")
-        print("********************************************\n"
+              "╚═╝     ╚═╝╚══════╝╚══════╝    ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝\n"
+              "********************************************\n"
               "//         created by:Yuval Kuina         //\n"
               "********************************************\n")
         
-        print("CLIENT CONNECTED!")
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serverSocket.bind(("0.0.0.0", 1234))
+        self.serverSocket.listen(1)
+        self.directoryDatabasePath = directoryPath
 
+        if not os.path.exists(self.directoryDatabasePath):
+            os.makedirs(self.directoryDatabasePath)
+            print(f"Files Directory '{self.directoryDatabasePath}' created.")
+        else:
+            print(f"Files Directory '{self.directoryDatabasePath}' already exists.")
         
+        print("Server is listening on port 1234...")
+        self.clientSocket, self.clientAddress = self.serverSocket.accept()
+        print(f"Client connected from {self.clientAddress}")
+
     def GetCommands(self):
         # The function gets commands from the client.
-        # The function returns 
-        data = None
         while True:
-            data = server.clientSocket.recv(1024).decode()
+            data = self.clientSocket.recv(1024).decode()
             if data == "1":
-                server.GetFileFromClient()
+                self.GetFileFromClient()
             elif data == "2":
-                server.SendFile()
+                self.SendFile()
             elif data == "3":
-                print("Disconnecting now...")
-                server.Disconnect()
+                self.Disconnect()
                 break
-        
-
-    def GetListOfFiles(self):
-        # The function gets nothing.
-        # The function returns a list of string which contain the names of all the files in the database directory.
-        try:
-            if os.path.exists(self.directoryDatabasePath):
-                filesList = os.listdir(self.directoryDatabasePath)
-            
-        except Exception as e:
-            print(f"An error occurred: {e}")
-        return []
+            else:
+                print("Invalid command received from client")
 
 
     def GetFileFromClient(self):
-        # The function gets nothing.
-        # The function insert the client's file to the server's database(directory of files)
-        # and send '1' if the files where uploaded successfully, otherwise '2'
-        try:
-            fileName = self.serverSocket.recv(4096).decode() 
-            fileContent = self.serverSocket.recv(4096).decode()
-            with open(self.directoryDatabasePath + "\\" + fileName, "w") as Newfile:
-                Newfile.write(fileContent)
-            self.clientSocket.send("1".encode())
-        except Exception:
-            self.clientSocket.send("2".encode())
+        # The function receives the file from the client and saves it to the server's directory
+
+        fileName = self.clientSocket.recv(1024).decode() 
+        fileContent = self.clientSocket.recv(4096).decode()
+        
+        file_path = os.path.join(self.directoryDatabasePath, fileName)
+        with open(file_path, "w") as file:
+            file.write(fileContent)
+        print(f"The file '{fileName}' successfully uploaded!")
 
 
     def SendFile(self):
-        # The function gets nothing.
-        # The function send the file, if it exists, from the database, otherwise None.
-        fileName = self.clientSocket.recv(4096).decode()
-        listOfFiles = self.GetListOfFiles()
-        fileContent = None
-        if fileName in listOfFiles:
-            fileContent = self.directoryDatabasePath + "\\" + fileName
-        self.clientSocket.send(fileContent.encode())
+        # The function sends a requested file to the client if it exists
         
-    
-    def Disconnect(self):
-        # The function gets nothing.
-        # The function close the sockets of the client and the server.
-        self.serverSocket.close()
-        self.clientSocket.close()
+        fileName = self.clientSocket.recv(1024).decode()
+        file_path = os.path.join(self.directoryDatabasePath, fileName)
+        
+        if os.path.exists(file_path):
+            self.clientSocket.send(fileName.encode())
+            with open(file_path, "r") as file:
+                self.clientSocket.send(file.read().encode())
+            print(f"The file '{fileName}' successfully sent!")
+        else:
+            self.clientSocket.send("File not found".encode())
 
+
+    def Disconnect(self):
+        # The function closes the server and client sockets
+        self.clientSocket.close()
+        self.serverSocket.close()
+        print("Disconnected!")
 
 if __name__ == "__main__":
-    # Define the folder path
-    folderPath = os.path.join(os.getcwd(), "My_Folder")
-
-    # Check if the folder exists
-    if not os.path.exists(folderPath):
-        os.makedirs(folderPath)
-        print(f"Files Directory '{folderPath}' created.")
-    else:
-        print(f"Files Directory '{folderPath}' already exists.")
-
     server = Server(directoryPath=os.path.join(os.getcwd(), "FilesDirectory"))
     server.GetCommands()
-    
-
-
-# server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server_socket.bind(("0.0.0.0", 1234))
-# server_socket.listen()
-# print("SERVER UP\nSERVER RUNNING")
-# (client_socket, client_address) = server_socket.accept()
-# print("CLIENT CONNECTED!")
-
-# # while True:
-# #     data = client_socket.recv(1024).decode()
-# #     print(f"Client: {data}")
-# #     # A known agreement to finish the connection with the client(if the client send the string "Quit,
-# #     # so close the connection with him by sending him the string "client shut down"
-# #     if data == "Quit":
-# #         print("Closing client socket now...")
-# #         client_socket.send("client shut down".encode())
-# #         break
-# #     elif data in server_commands_dictionary:
-# #         data = server_commands_dictionary[data]
-
-# #     client_socket.send(str(data).encode())
-
