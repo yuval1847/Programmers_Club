2b4ea53988aeb1b916c2db3de91e64151eb43a1a
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2024-06-19 19:35:55 +0300

    Update client.py

--- Networking/Files_Managment/CLI_Version/client.py
+++ Networking/Files_Managment/CLI_Version/client.py
@@ -4,6 +4,7 @@ import os
 class Client:
     """A class which represents a client."""
 
+
     def __init__(self):
         print("███████╗██╗██╗     ███████╗     ██████╗██╗     ██╗███████╗███╗   ██╗████████╗\n"
               "██╔════╝██║██║     ██╔════╝    ██╔════╝██║     ██║██╔════╝████╗  ██║╚══██╔══╝\n"
@@ -27,7 +28,6 @@ class Client:
         print("Menu:\n1. Upload File\n2. Download File\n3. Quit")
         while True:
             command = input("Enter your option: ")
-
             if command == "1":
                 self.UploadFile()
             elif command == "2":
@@ -46,37 +46,58 @@ class Client:
         while not os.path.exists(filePath):
             print("The given path does not exist! Please try again.")
             filePath = input("Enter a valid file path: ")
-
+        
+        # send the name of the file
         self.clientSocket.send("1".encode())
         fileName = os.path.basename(filePath)
         self.clientSocket.send(fileName.encode())
 
-        with open(filePath, "r") as file:
-            self.clientSocket.send(file.read().encode())
+        # send the size of the file
+        self.clientSocket.send(str(os.stat(filePath).st_size).encode('utf8'))
+        #print(os.stat(filePath).st_size)
+
+        with open(filePath, "rb") as file:
+            self.clientSocket.send(file.read())
         print(f"The file '{fileName}' successfully uploaded.")
 
 
     def DownloadFile(self):
-        # The function requests a file from the server and saves it locally
+        # The function presents the user the list of files from the database
+        # and requests a file from the server and saves it locally.
         self.clientSocket.send("2".encode())
+
+        filesNamesLength = int(str(self.clientSocket.recv(1024).decode("utf8")))
+        filesNamesFromDatabase = (self.clientSocket.recv(filesNamesLength).decode()).split("*")[1:]
+        maxLenOfName = 0
+        for i in filesNamesFromDatabase:
+            if len(i) > maxLenOfName:
+                maxLenOfName = len(i)
+        print(maxLenOfName-len(filesNamesFromDatabase[0]))
+        print("======Database Files======")
+        for i in range(len(filesNamesFromDatabase)):
+            print("| -file- "+filesNamesFromDatabase[i]+" "*(maxLenOfName-len(filesNamesFromDatabase[i]))+' |')
+        print("==========================")
+
         fileName = input("Enter the file name to download: ")
         self.clientSocket.send(fileName.encode())
 
         response = self.clientSocket.recv(1024).decode()
+        fileSize = None
         if response == "File not found":
             print("The requested file does not exist on the server.")
         else:
-            with open(fileName, "w") as file:
-                file.write(self.clientSocket.recv(4096).decode())
+            fileSize = int(str(self.clientSocket.recv(1024).decode("utf8")))
+            with open(fileName, "wb") as file:
+                file.write(self.clientSocket.recv(fileSize))
             print(f"File '{fileName}' successfully downloaded.")
 
     
     def Disconnect(self):
-        # The function gets nothing.
-        # The function disconnect the client's socket.
+        # The function disconnects the client's socket.
         self.clientSocket.send("3".encode())
         self.clientSocket.close()
         print("Disconnected from the server.")
+
 
 if __name__ == "__main__":
     client = Client()
