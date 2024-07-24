cd0ffbde95b12a0f8ab7c2420a67f601782f4025
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2024-07-24 12:13:12 +0300

    Delete OOP/Students_Managment directory

--- OOP/Students_Managment/IPerson.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace IPersonNameSpace{
-    class interface IPerson
-    {
-        // An interface class which represent a person's functions.
-        void DisplayInformation();
-    }
-}
