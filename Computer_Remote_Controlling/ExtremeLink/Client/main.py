173c2bd2f942ae75059f1f144bcee311dfa8cb06
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2024-09-05 14:06:03 +0300

    Add files via upload

---
+++ Computer_Remote_Controlling/ExtremeLink/Client/main.py
@@ -0,0 +1,8 @@
+import Pages_Client.Pages_Manager
+
+def main():
+    PagesManagerObject = Pages_Client.Pages_Manager.PagesManager()
+    PagesManagerObject.mainloop()
+    
+if __name__ == "__main__":
+    main()
