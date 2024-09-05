e15d0f9ecdd7bc55381c653c6f01a74b0eadd0c9
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2024-09-05 14:05:36 +0300

    Delete Computer_Remote_Controlling/ExtremeLink directory

--- Computer_Remote_Controlling/ExtremeLink/Server/main.py
+++
@@ -1,8 +0,0 @@
-import Pages_Server.Pages_Manager
-
-def main():
-    PagesManagerObject = Pages_Server.Pages_Manager.PagesManager()
-    PagesManagerObject.mainloop()
-    
-if __name__ == "__main__":
-    main()
