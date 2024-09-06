4a7ee5efcfa262851d7785e2dafda8ffda4e723c
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2024-09-06 19:32:56 +0300

    Add files via upload

---
+++ Computer_Remote_Controlling/ExtremeLink/Client/Pages_Client/Pages_Manager.py
@@ -0,0 +1,26 @@
+import customtkinter
+import Pages_Client.Start_Page
+import Pages_Client.Connecting_Page
+
+class PagesManager(customtkinter.CTk):
+    """A class which manage the pages in the program"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.geometry("960x600")
+        self.resizable(False, False)
+        self.configure(bg="gray24")
+        self.title("ExtremeLink v1.0 Server")
+
+        self.frames = {"StartPage": Pages_Client.Start_Page.StartPage(master=self),
+                       "ConnectingPage": Pages_Client.Connecting_Page.ConnectingPage(master=self)}  
+             
+        self.update_idletasks() 
+
+        self.ShowFrame("StartPage")
+
+
+    def ShowFrame(self, pageName):
+        # The function gets a page name.
+        # The function shows on the self the right page according to the given number.
+        self.frames[pageName].pack(fill = "both", expand = True)
