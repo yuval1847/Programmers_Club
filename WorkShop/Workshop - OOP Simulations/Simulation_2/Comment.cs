183c53aff3ba25312bc58ee38c18e1841394fe8b
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2024-11-17 21:08:06 +0200

    Add files via upload

---
+++ WorkShop/Workshop - OOP Simulations/Simulation_2/Comment.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simulation_2.Post_Properties_Classes
+{
+    public class Comment
+    {
+        
+    }
+}
